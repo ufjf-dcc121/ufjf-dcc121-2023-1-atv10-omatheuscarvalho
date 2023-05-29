1fd52717ff84e3e4811596c879e6d1769a480588
Matheus Carvalho <matheusc200@gmail.com>
2023-05-29 15:23:16 -0300

    Finalizando o exercicio

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', setup);
 function setup() {
     const form = document.forms.entrada;
     form.addEventListener('submit', envia);
-
+    
     function envia(evento) {
         evento.preventDefault();
         console.log('Formulário enviado!');
