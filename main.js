e2b4cc67819e088b27f521de4bba567d402f7b43
Matheus Carvalho <matheusc200@gmail.com>
2023-05-29 15:16:06 -0300

    Funcionalidade do formulario

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', setup);
+
+function setup() {
+    const form = document.forms.entrada;
+    form.addEventListener('submit', envia);
+
+    function envia(evento) {
+        evento.preventDefault();
+        console.log('Formulário enviado!');
+    }
+}
